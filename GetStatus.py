4154a0a85617ba9a412152c34789d9b245ada462
tomato <nomu.tomato.zyusu@gmail.com>
2017-11-15 14:21:06 +0900

    [add]クラス図(仮)の追加及びメソッド名変更とメソッド追加

--- GetStatus.py
+++ GetStatus.py
@@ -2,15 +2,21 @@ from bs4 import BeautifulSoup
 
 class GetStatus:
     def __init__(self,uri):
+        self.uri = uri
         html_text = open(uri,'r')
         self.soup = BeautifulSoup(html_text.read(),'html.parser')
 
-    def getStatus(self):
+    def getTrainStatus(self):
         return self.connectWebToGetStatus()
 
     def connectWebToGetStatus(self):
         contents = self.soup.find('div', id="mdServiceStatus")
         print(contents.text)
 
+    def getTitle(self):
+        pass #todo
 
+    def update(self):
+        html_text = open(self.uri,'r')
+        self.soup = BeautifulSoup(html_text.read(),'html.parser')
 
