f0f75c5dab790a6ce8d78a452a68f102f48e4e85
tomato <nomu.tomato.zyusu@gmail.com>
2017-11-15 17:31:44 +0900

    [add]ontimeかどうかを返す辞書追加

--- GetStatus.py
+++ GetStatus.py
@@ -14,6 +14,7 @@ class GetStatus:
         contents = self.soup.find('div', id="mdServiceStatus")
         onTime = self.isOnTime(contents)
         result['status'] = contents.find('p').text
+        result['isOntime'] = onTime
         result['lineName'] = self.getLineTitle(contents)
         return result
 
