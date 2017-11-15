a332feea712fcb520a882aa55607e165c22d795a
tomato <nomu.tomato.zyusu@gmail.com>
2017-11-16 07:54:07 +0900

    [add]とりあえず骨組みだけファイル追加

---
+++ StatusTelop.py
@@ -0,0 +1,12 @@
+import GetStatus as gs
+
+class StatusTelop:
+    def __init__(self):
+       self.sutasues = []
+
+    def addLine(self,url):
+        pass
+
+    def __str__(self):
+        pass
+
