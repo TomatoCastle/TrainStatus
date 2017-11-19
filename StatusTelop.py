3aefc43026c264eff6197989d019aff4e0882475
tomato <nomu.tomato.zyusu@gmail.com>
2017-11-19 22:22:53 +0900

    [add]型例外追加

--- StatusTelop.py
+++ StatusTelop.py
@@ -5,12 +5,16 @@ class StatusTelop:
        self.sutasues = []
 
     def addLine(self,uri):
+        self.checkTypeUri(uri)
         self.sutasues.append(gs.GetStatus(uri))
 
     def __str__(self):
         str = ""
         for i in self.sutasues:
             sutatsu = i.getTrainStatus()
-            str += '【' + sutatsu['lineName'] + '】 ' + sutatsu['status'] + "   "
+            str += '【' + sutatsu['lineName'] + '】 ' + sutatsu['status'] + "        "
         return str
 
+    def checkTypeUri(self,uri):
+        if not isinstance(uri,str):
+            raise TypeError('\"uri\"\'s type must be \"str\"')
